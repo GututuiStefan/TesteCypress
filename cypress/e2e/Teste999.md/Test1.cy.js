ba176ffd3b635c7ab12296c28c251763f5ebe286
HP/Stefan\user <gututui87@gmail.com>
2023-02-28 12:18:53 +0200

    initial commit

---
+++ cypress/e2e/Teste999.md/Test1.cy.js
@@ -0,0 +1,73 @@
+describe("Site 999.md", () => {
+    //Test1
+//     it("Functioneaza cu o cautare basic + Sortarea si schimb de valuta", () => {
+//         cy.visit("https://999.md/ro/");
+//         cy.get(".twitter-typeahead").type("toyota").type("{enter}");
+//         cy.get('.search-filters__form__item__header').click();
+//         cy.get('.last > span').should("exist");
+//         cy.get('.search-filters__wrapper > :nth-child(3)').click(); 
+//         cy.get('[for="filter-price_desc"]').click();       
+//         cy.get('.items__header__currencies > :nth-child(3)').click();
+//         cy.get(".ads-list-photo-item-price-wrapper").should('be.visible')
+//     })
+
+//    Test2
+    // it('Verificare valorii din input', ()=>{
+    //     cy.visit("https://999.md/ro/");
+    //     cy.get('#js-search-input').click();
+
+    //     const search = cy.get("#js-search-input");
+    //     search.type('1234567890');
+    //     search.should('have.value','1234567890');
+        
+    // })
+
+
+    //  Test3
+
+    //  it('Verificam daca butonul "Facebook" din Footer, are o anumita valoare din input', () => {
+    //     cy.visit('https://999.md/ro');
+    //     cy.get('[href="https://www.facebook.com/www999md/"]').should('have.class', 'is-fb');
+        
+
+    // // Aici am incercat si varianta cu constanta
+    
+    //     cy.get('[href="https://www.facebook.com/www999md/"]');                         
+    //     const classFB = cy.get('[href="https://www.facebook.com/www999md/"]');
+    //     classFB.should('have.class', 'is-fb');
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Test Screenshot
+
+
+
+
+
+
+
+
+
+
+
+})
